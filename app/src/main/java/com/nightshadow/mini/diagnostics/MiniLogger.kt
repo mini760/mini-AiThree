03efb58af0d0268b4cb33e172a322dcf863d91b1
mini760 <minitheaiassistant@gmail.com>
2026-09-05 21:02:41 +0530

    Implement MiniLogger for logging with global tag

--- app/src/main/java/com/nightshadow/mini/diagnostics/MiniLogger.kt
+++ app/src/main/java/com/nightshadow/mini/diagnostics/MiniLogger.kt
@@ -1,1 +1,27 @@
+package com.nightshadow.mini.diagnostics
 
+import android.util.Log
+
+object MiniLogger {
+    private const val GLOBAL_TAG = "MiniAgent"
+
+    fun init() {
+        i("System", "MiniLogger initialized")
+    }
+
+    fun d(tag: String, message: String) {
+        Log.d("$GLOBAL_TAG-$tag", message)
+    }
+
+    fun i(tag: String, message: String) {
+        Log.i("$GLOBAL_TAG-$tag", message)
+    }
+
+    fun w(tag: String, message: String) {
+        Log.w("$GLOBAL_TAG-$tag", message)
+    }
+
+    fun e(tag: String, message: String, throwable: Throwable? = null) {
+        Log.e("$GLOBAL_TAG-$tag", message, throwable)
+    }
+}
